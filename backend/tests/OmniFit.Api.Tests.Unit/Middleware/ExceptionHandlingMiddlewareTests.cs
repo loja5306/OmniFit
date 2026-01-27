a0be094a6ecbf51649f50a46d77ad1c1099e21fc
loja5306 <lukeatkinson0601@gmail.com>
2026-01-27 19:54:04 +0000

    Adding unit tests for the Exception Handling Middleware

---
+++ backend/tests/OmniFit.Api.Tests.Unit/Middleware/ExceptionHandlingMiddlewareTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Http;
+using OmniFit.Api.Middleware;
+
+namespace OmniFit.Api.Tests.Unit.Middleware
+{
+    public class ExceptionHandlingMiddlewareTests
+    {
+        private readonly HttpContext _httpContext = new DefaultHttpContext();
+
+        [Fact]
+        public async Task InvokeAsync_ShouldReturn500_WhenGenericExceptionThrown()
+        {
+            // Arrange
+            RequestDelegate next = (ctx) => throw new Exception("An error occurred");
+            var sut = new ExceptionHandlingMiddleware(next);
+
+            // Act
+            await sut.InvokeAsync(_httpContext);
+
+            //Assert
+            _httpContext.Response.StatusCode.Should().Be(StatusCodes.Status500InternalServerError);
+        }
+
+        [Fact]
+        public async Task InvokeAsync_ShouldReturn400_WhenValidationExceptionThrown()
+        {
+            // Arrange
+            RequestDelegate next = (ctx) => throw new ValidationException(new List<ValidationFailure> { new ValidationFailure("Name", "Workout name is required") });
+            var sut = new ExceptionHandlingMiddleware(next);
+
+            // Act
+            await sut.InvokeAsync(_httpContext);
+
+            //Assert
+            _httpContext.Response.StatusCode.Should().Be(StatusCodes.Status400BadRequest);
+        }
+    }
+}
