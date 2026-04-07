e3402727453efe3b073458462c97f5ccc7aa1ddd
loja5306 <lukeatkinson0601@gmail.com>
2026-04-07 23:48:55 +0100

    Adding S3 & CloudFront to terraform infrastructure for frontend

---
+++ infrastructure/cloudfront.tf
@@ -0,0 +1,40 @@
+resource "aws_cloudfront_distribution" "frontend" {
+  enabled = true
+  default_root_object = "index.html"
+
+  origin {
+    domain_name = aws_s3_bucket.frontend.bucket_regional_domain_name
+    origin_id = "omnifit-s3"
+    origin_access_control_id = aws_cloudfront_origin_access_control.frontend.id
+  }
+
+  default_cache_behavior {
+    target_origin_id = "omnifit-s3"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+resource "aws_cloudfront_origin_access_control" "frontend" {
+  name = "omnifit-frontend-oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior = "always"
+  signing_protocol = "sigv4"
+}
---
+++ infrastructure/s3.tf
@@ -0,0 +1,30 @@
+resource "aws_s3_bucket" "frontend" {
+  bucket = "omnifit-frontend"
+
+  tags = {
+    Name = "omnifit-frontend"    
+  }
+}
+
+resource "aws_s3_bucket_policy" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.frontend.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.frontend.arn
+          }
+        }
+      }
+    ]
+  })
+}
