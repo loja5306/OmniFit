2adfb4fdcb4d0b1ac66bd1523537d01fcab030da
loja5306 <lukeatkinson0601@gmail.com>
2026-04-14 17:23:11 +0100

    Adding logo and favicon

--- frontend/src/components/common/NavBar.tsx
+++ frontend/src/components/common/NavBar.tsx
@@ -1,38 +1,39 @@
-import { Link } from "react-router"
+import { Link } from "react-router";
 import { useAuth } from "../../contexts/AuthContext";
+import logo from "../../assets/omnifit-logo.svg";
 
 const NavBar = () => {
   const { isAuthenticated, email, logout } = useAuth();
 
   return (
-    <nav className="w-full flex justify-between items-center p-4 bg-blue-400 text-white">
-        <div className="flex justify-center items-center gap-4">
-          <Link to="/" className="text-2xl font-bold">
-            OmniFit
-          </Link>
-          <div className="w-px h-8 bg-white/50" />
-          <div className="flex items-center space-x-4">
-            <Link to="/workouts">Workouts</Link>
-            <Link to="/exercises">Exercises</Link>
-          </div>
+    <nav className="w-full flex justify-between items-center p-4 bg-white shadow-sm text-primary font-semibold text-lg">
+      <div className="flex justify-center items-center gap-4">
+        <Link to="/" className="text-2xl font-bold">
+          <img src={logo} alt="OmniFit" height={2} />
+        </Link>
+        <div className="w-px h-10 bg-gray-200" />
+        <div className="flex items-center space-x-4">
+          <Link to="/workouts">Workouts</Link>
+          <Link to="/exercises">Exercises</Link>
         </div>
-        <div className="border border-white/50 rounded-lg px-4 py-2 space-x-4">
-          {isAuthenticated ? (
-            <>
-              <span className="font-medium">{email}</span>
-              <button onClick={logout} className="cursor-pointer hover:underline">
-                Logout
-              </button>
-            </>
-          ) : (
-            <>
-              <Link to="/login">Login</Link>
-              <Link to="/register">Register</Link>
-            </>
-          )}
-        </div>
-      </nav>
-  )
-}
+      </div>
+      <div className="border border-gray-200 shadow-sm rounded-lg px-4 py-2 space-x-4">
+        {isAuthenticated ? (
+          <>
+            <span className="font-medium">{email}</span>
+            <button onClick={logout} className="cursor-pointer hover:underline">
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </div>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
